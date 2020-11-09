763aa0547bf2a82a2e6652f5fe15d731496248c9
deanlauvina7 <deanlauvina7@gmail.com>
2020-11-09 17:57:41 -0600

    Hero section finished

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { NavLink as Link } from 'react-router-dom';
+import { FaPizzaSlice } from 'react-icons/fa';
 
 export const Nav = styled.nav`
   background: transparent;
@@ -36,4 +37,9 @@ export const NavIcon = styled.div`
     transform: translate(-175%, 100%);
     font-weight: bold;
   }
+`;
+
+export const Bars = styled(FaPizzaSlice)`
+  font-size: 2rem;
+  transform: translate(-50%, -15%);
 `;
