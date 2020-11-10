6ca259717562f5d722da7241fdac6f5c0a2fcc55
deanlauvina7 <deanlauvina7@gmail.com>
2020-11-09 20:28:37 -0600

    Finiished the feature section

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import { GlobalStyle } from './globalStyles';
 import Hero from './components/Hero';
 import { productData } from './components/Products/data';
 import Products from './components/Products';
-
+import Feature from './components/Feature';
 function App() {
   return (
     <Router>
       <GlobalStyle />
       <Hero />
       <Products heading="Choose your favorite" data={productData} />
+      <Feature />
     </Router>
   );
 }
